9557ec0057d5912fa4a6de11cdf1631983e5fa51
jacksonode@dedaluswine.com <jacksonode@dedaluswine.com>
2023-02-28 21:34:37 +0000

    folder rename

--- models/staging/stg_eventbrite.sql
+++ models/staging/stg_eventbrite.sql
@@ -1,3 +1,22 @@
+
+--checklist: 
+--1. Find style guide for stg files
+--2. this should be a select * statement
+--3. once you find the instructions, paste the URL for documentation purposes. 
+
+Order_ID
+Order_Date
+Attendee_Status
+Name
+Email
+Event_Name
+Ticket_Quantity
+Ticket_Type
+Ticket_Price
+Buyer_Name
+Buyer_Email
+
+
 with eventbrite_customers as (
     
     select *
